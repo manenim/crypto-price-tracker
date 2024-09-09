44e16cc847161b52f179e45b322b8550e9b00c93
Manenimabasi Udoh (Dev Mani) <85757137+manenim@users.noreply.github.com>
2024-09-09 12:01:11 +0100

    fix documentation error

--- src/alerts/alerts.controller.ts
+++ src/alerts/alerts.controller.ts
@@ -10,20 +10,31 @@ import {
 import { AlertsService } from './alerts.service';
 import { CreateAlertDto } from './dto/create-alert.dto';
 import { UpdateAlertDto } from './dto/update-alert.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+
+ApiTags('alerts');
 @Controller('alerts')
 export class AlertsController {
   constructor(private readonly alertsService: AlertsService) {}
 
+  @ApiOperation({ summary: 'Create a new alert' })
+  @ApiBody({ type: CreateAlertDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The alert has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
   @Post('/set-alert')
   create(@Body() createAlertDto: CreateAlertDto) {
     return this.alertsService.create(createAlertDto);
   }
 
+  @ApiOperation({ summary: 'Get all alerts' })
+  @ApiResponse({ status: 200, description: 'Returns all alerts.' })
   @Get()
   findAll() {
     return this.alertsService.findAll();
   }
-
-
 }
+
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Healthcheck' })
+  @ApiResponse({
+    status: 201,
+  })
+  @ApiResponse({ status: 400, description: 'API is up and running' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
