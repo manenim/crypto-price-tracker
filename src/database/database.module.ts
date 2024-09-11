bef64c850107293f1ad35e6b5e2e37c5f4d36f74
Manenimabasi Udoh (Dev Mani) <85757137+manenim@users.noreply.github.com>
2024-09-11 07:14:27 +0100

    feat: port binding

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,11 +7,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
+        // url: 'postgresql://mani:kjm57EwyEPqKfietDDhdxEpNg7OFe9v3@dpg-crga085svqrc73f0h5c0-a.oregon-postgres.render.com/cryptodb_l4dd',
         host: configService.getOrThrow('DB_HOST'),
         port: configService.getOrThrow('DB_PORT'),
         username: configService.getOrThrow('DB_USERNAME'),
         password: configService.getOrThrow('DB_PASSWORD'),
         database: configService.getOrThrow('DB_NAME'),
+        ssl: true,
         autoLoadEntities: true,
         synchronize: true,
       }),
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,8 +19,9 @@ async function bootstrap() {
      .build();
    const document = SwaggerModule.createDocument(app, config);
    SwaggerModule.setup('docs', app, document);
+  const configService = app.get(ConfigService);
 
 
-  await app.listen(3000);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
